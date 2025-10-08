512f79e4c49b3d1dfba9049a40e5ef5d406a1c3f
4simo8-cyber <4.simo.8@gmail.com>
2025-10-08 08:26:36 +0330

    Update query.py

--- query.py
+++ query.py
@@ -3,45 +3,37 @@ import os
 from dotenv import load_dotenv
 from db import db_url
 
-
+load_dotenv()  # بارگذاری متغیرهای محیطی
 
 def con():
-    conn=psycopg2.connect(db_url)
-    cursor=conn.cursor()
-    return conn,cursor
+    conn = psycopg2.connect(db_url)
+    return conn
 
-def insert(name , price , stock,photo):
-    conn,cursor=con()
-    cursor.execute('''INSERT INTO product (name, price, stock, photo) VALUES (%s, %s, %s, %s) ON CONFLICT (name) DO UPDATE SET price = EXCLUDED.price, stock = EXCLUDED.stock, photo = EXCLUDED.photo''', (name, price, stock, psycopg2.Binary(photo)))
-    
-    conn.commit()
-    conn.close()
+def insert(name, price, stock, photo):
+    with con() as conn:
+        with conn.cursor() as cursor:
+            cursor.execute('''INSERT INTO product (name, price, stock, photo) VALUES (%s, %s, %s, %s) ON CONFLICT (name) DO NOTHING''', 
+                           (name, price, stock, psycopg2.Binary(photo)))
 
 def select():
-    conn,cursor=con()
-    cursor.execute('''SELECT name,price,stock,photo FROM product''')
-    datas=cursor.fetchall()
-    conn.commit()
-    conn.close()
-    return datas
+    with con() as conn:
+        with conn.cursor() as cursor:
+            cursor.execute('''SELECT name, price, stock, photo FROM product''')
+            return cursor.fetchall()
 
-def update(name,a):
-    conn,cursor=con()
-    cursor.execute('''UPDATE product SET stock=%s WHERE name=%s''', (a,name))
-
-    conn.commit()
-    conn.close()
+def update(name, a):
+    with con() as conn:
+        with conn.cursor() as cursor:
+            cursor.execute('''UPDATE product SET stock = %s WHERE name = %s''', (a, name))
 
 def delete(name):
-    conn,cursor=con()
-    cursor.execute('''DELETE FROM product WHERE name=%s''', (name,))
-    conn.commit()
-    conn.close()
+    with con() as conn:
+        with conn.cursor() as cursor:
+            cursor.execute('''DELETE FROM product WHERE name = %s''', (name,))
 
 def photos(name):
-    conn,cursor=con()
-    cursor.execute('''SELECT photo FROM product WHERE name=%s''',(name,))
-    data=cursor.fetchone()
-    conn.commit()
-    conn.close()
-    return data
+    with con() as conn:
+        with conn.cursor() as cursor:
+            cursor.execute('''SELECT photo FROM product WHERE name = %s''', (name,))
+            return cursor.fetchone()
+            
